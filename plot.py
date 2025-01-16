3f74de53bbdc91464390fc416a353aded3303332
ABoldetti <a.boldetti@campus.unimib.it>
2025-01-16 18:23:52 +0100

    Update Relazione document structure and enhance plot functionality

--- plot.py
+++ plot.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('Elettronica/MATLAB/hist_phot_3.4V.csv' , header=None)
 
 plt.hist( df.to_numpy().T , 100)
+#plt.show()
+plt.clf()
+
+df1 = pd.read_csv('Elettronica/MATLAB/phot_3.4V.csv' , header=None)
+
+df1 = df1.to_numpy()
+for i in df1:
+    print(i)
+    plt.plot( np.linspace(0, 1, len(i)), i)
 plt.show()
